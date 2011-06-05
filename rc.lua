6f54084572bdc49c7d4fbbdcb053871a60612052
Thomas Schreiber <tom@insatsu.us>
2011-06-05 21:46:40 +0200

    remove volume for now

--- rc.lua
+++ rc.lua
@@ -198,32 +198,6 @@ orgwidget:buttons(awful.util.table.join(
 ))
 -- }}}
 
--- {{{ Volume level
-volicon = widget({ type = "imagebox" })
-volicon.image = image(beautiful.widget_vol)
--- Initialize widgets
-volbar    = awful.widget.progressbar()
-volwidget = widget({ type = "textbox" })
--- Progressbar properties
-volbar:set_vertical(true):set_ticks(true)
-volbar:set_height(12):set_width(8):set_ticks_size(2)
-volbar:set_background_color(beautiful.fg_off_widget)
-volbar:set_gradient_colors({ beautiful.fg_widget,
-   beautiful.fg_center_widget, beautiful.fg_end_widget
-}) -- Enable caching
-vicious.cache(vicious.widgets.volume)
--- Register widgets
-vicious.register(volbar,    vicious.widgets.volume,  "$1",  2, "PCM")
-vicious.register(volwidget, vicious.widgets.volume, " $1%", 2, "PCM")
--- Register buttons
-volbar.widget:buttons(awful.util.table.join(
-   awful.button({ }, 1, function () exec("kmix") end),
-   awful.button({ }, 4, function () exec("amixer -q set PCM 2dB+", false) end),
-   awful.button({ }, 5, function () exec("amixer -q set PCM 2dB-", false) end)
-)) -- Register assigned buttons
-volwidget:buttons(volbar.widget:buttons())
--- }}}
-
 -- {{{ Date and time
 dateicon = widget({ type = "imagebox" })
 dateicon.image = image(beautiful.widget_date)
@@ -283,7 +257,6 @@ for s = 1, scount do
         },
         s == 1 and systray or nil,
         separator, datewidget, dateicon,
-        separator, volwidget,  volbar.widget, volicon,
         separator, orgwidget,  orgicon,
         separator, mailwidget, mailicon,
         separator, upicon,     netwidget, dnicon,
